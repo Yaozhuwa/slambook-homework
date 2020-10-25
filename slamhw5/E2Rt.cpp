4c6380c216f80bd4774cc42913ba213d58ebc559
yyz <yaozuye@zju.edu.cn>
2020-10-25 23:01:45 +0800

    hw5 1-3 Done.

--- slamhw5/E2Rt.cpp
+++ slamhw5/E2Rt.cpp
@@ -6,6 +6,7 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
+#include <cmath>
 
 using namespace Eigen;
 
@@ -29,6 +30,14 @@ int main(int argc, char **argv) {
 
     // SVD and fix sigular values
     // START YOUR CODE HERE
+    JacobiSVD<MatrixXd> svd(E, ComputeThinU | ComputeThinV);
+    Matrix3d U = svd.matrixU();
+    Matrix3d V = svd.matrixV();
+    Vector3d sigma_vec = svd.singularValues();
+    Matrix3d sigma_mat = Matrix3d::Zero();
+    sigma_mat(0,0) = (sigma_vec(0)+sigma_vec(1))/2;
+    sigma_mat(1,1) = (sigma_vec(0)+sigma_vec(1))/2;
+    Matrix3d R90 = AngleAxisd(M_PI/2, Vector3d(0,0,1)).toRotationMatrix();
 
     // END YOUR CODE HERE
 
@@ -36,9 +45,13 @@ int main(int argc, char **argv) {
     // START YOUR CODE HERE
     Matrix3d t_wedge1;
     Matrix3d t_wedge2;
-
     Matrix3d R1;
     Matrix3d R2;
+    t_wedge1 = U*R90*sigma_mat*U.transpose();
+    R1 = U*R90.transpose()*V.transpose();
+
+    t_wedge2 = U*R90.transpose()*sigma_mat*U.transpose();
+    R2 = U*R90*V.transpose();
     // END YOUR CODE HERE
 
     cout << "R1 = " << R1 << endl;
--- slamhw5/GN-BA.cpp
+++ slamhw5/GN-BA.cpp
@@ -12,7 +12,7 @@ using namespace Eigen;
 #include <iostream>
 #include <iomanip>
 
-#include "sophus/se3.h"
+#include "sophus/se3.hpp"
 
 using namespace std;
 
@@ -20,8 +20,8 @@ typedef vector<Vector3d, Eigen::aligned_allocator<Vector3d>> VecVector3d;
 typedef vector<Vector2d, Eigen::aligned_allocator<Vector3d>> VecVector2d;
 typedef Matrix<double, 6, 1> Vector6d;
 
-string p3d_file = "./p3d.txt";
-string p2d_file = "./p2d.txt";
+string p3d_file = "../p3d.txt";
+string p2d_file = "../p2d.txt";
 
 int main(int argc, char **argv) {
 
@@ -33,7 +33,18 @@ int main(int argc, char **argv) {
 
     // load points in to p3d and p2d 
     // START YOUR CODE HERE
-
+    ifstream f3d(p3d_file);
+    ifstream f2d(p2d_file);
+    while(!f2d.eof()){
+        double u,v;
+        f2d>>u>>v;
+        p2d.push_back(Vector2d(u,v));
+    }
+    while(!f3d.eof()){
+        double x,y,z;
+        f3d>>x>>y>>z;
+        p3d.push_back(Vector3d(x,y,z));
+    }
     // END YOUR CODE HERE
     assert(p3d.size() == p2d.size());
 
@@ -42,7 +53,7 @@ int main(int argc, char **argv) {
     int nPoints = p3d.size();
     cout << "points: " << nPoints << endl;
 
-    Sophus::SE3 T_esti; // estimated pose
+    Sophus::SE3d T_esti; // estimated pose
 
     for (int iter = 0; iter < iterations; iter++) {
 
@@ -54,14 +65,31 @@ int main(int argc, char **argv) {
         for (int i = 0; i < nPoints; i++) {
             // compute cost for p3d[I] and p2d[I]
             // START YOUR CODE HERE 
+            Vector4d p4d;
+            p4d<<p3d[i], 1;
+            Vector4d p_t = T_esti * p4d;
+            Vector3d p_k = K*p_t.head(3);
+            Vector2d e = p2d[i] - Vector2d(p_k[0]/p_k[2], p_k[1]/p_k[2]);
+            cost += e.squaredNorm()/2; 
+	        // END YOUR CODE HERE
 
-	    // END YOUR CODE HERE
-
-	    // compute jacobian
+	        // compute jacobian
             Matrix<double, 2, 6> J;
             // START YOUR CODE HERE 
-
-	    // END YOUR CODE HERE
+            J(0,0) = fx/p_t[2];
+            J(0,1) = 0;
+            J(0,2) = -fx*p_t[0]/p_t[2]/p_t[2];
+            J(0,3) = -fx*p_t[0]*p_t[1]/p_t[2]/p_t[2];
+            J(0,4) = fx + fx*p_t[0]*p_t[0]/p_t[2]/p_t[2];
+            J(0,5) = -fx*p_t[1]/p_t[2];
+            J(1,0) = 0;
+            J(1,1) = fy/p_t[2];
+            J(1,2) = -fx*p_t[1]/p_t[2]/p_t[2];
+            J(1,3) = -fy-fy*p_t[1]*p_t[1]/p_t[2]/p_t[2];
+            J(1,4) = fy*p_t[0]*p_t[1]/p_t[2]/p_t[2];
+            J(1,5) = fy*p_t[0]/p_t[2];
+            J = -J;
+	        // END YOUR CODE HERE
 
             H += J.transpose() * J;
             b += -J.transpose() * e;
@@ -71,7 +99,7 @@ int main(int argc, char **argv) {
         Vector6d dx;
 
         // START YOUR CODE HERE 
-
+        dx=H.ldlt().solve(b);
         // END YOUR CODE HERE
 
         if (isnan(dx[0])) {
@@ -87,7 +115,7 @@ int main(int argc, char **argv) {
 
         // update your estimation
         // START YOUR CODE HERE 
-
+        T_esti=Sophus::SE3d::exp(dx)*T_esti;
         // END YOUR CODE HERE
         
         lastCost = cost;
